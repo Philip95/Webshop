c8b4fca8506b2ba65412d353d91bb0e10c5a3d4d
Philip95 <philip.strobach@gmail.com>
2020-06-07 15:23:48 +0200

    add laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.js('resources/js/bootstrap.js', 'public/js');
+mix.styles('resources/css/css.css', 'public/css/all.css');
+mix.js('resources/js/webshop.js', 'public/js');
